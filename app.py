9dfbd8b7939f707b6eb03b33f5c5173ce1149ece
vbommena <vishaal.bommena@gmail.com>
2018-09-30 01:01:17 -0400

    Update Application

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def sendEmail(email_id):
     content = Content("text/plain", "Hello, Email!")
     mail = Mail(from_email, subject, to_email, content)
     response = sg.client.mail.send.post(request_body=mail.get())
-    print response.status_code 
+    print response.status_code
     print response.body
     print response.headers
     return jsonify("Sent")
