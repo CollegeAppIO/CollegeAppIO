4f3cddc9115972df7f21bec6e673e740e3f7635f
Harsha Lingareddy <hlingare@purdue.edu>
2018-11-30 20:39:06 -0500

    added new fucntionaly for dynaicm quesitons

--- Frontend/collegeAppFrontEnd/src/app/college-specific-page/college-specific-page.component.ts
+++ Frontend/collegeAppFrontEnd/src/app/college-specific-page/college-specific-page.component.ts
@@ -26,9 +26,10 @@ export class CollegeSpecificPageComponent implements OnInit {
   collegeName: string;
   message:string;
   testarea:string;
-  q1: string;
-  q2: string;
-  q3: string;
+  
+  questions: any;
+  answers: string[] = [];
+
   major: string;
   uid:string;
   email:string;
@@ -46,7 +47,7 @@ export class CollegeSpecificPageComponent implements OnInit {
 
         var temp = 'http://college-app-io.herokuapp.com/getStudentResponse';
         this.httpClient.get(temp,{headers: {'collegeName': this.message, 'studentid':this.uid}}).subscribe(data => {
-              // this.userData = data[0] as JSON;
+              
 
               if(data == 'Student Already Applied'){
                 this.isDisabled = true;
@@ -54,12 +55,26 @@ export class CollegeSpecificPageComponent implements OnInit {
               }else{
                 this.isDisabled = false;
               }
-              this.q1 = data[0].q1;
-              this.q2 = data[0].q2;
-              this.q3 = data[0].q3;
+              
+              console.log(data[0].major);
+              console.log(data[0].questions);
+
               this.major = data[0].major;
+              this.answers = data[0].questions;
+              
+
+              // this.q1 = data[0].q1;
+              // this.q2 = data[0].q2;
+              // this.q3 = data[0].q3;
+              // this.major = data[0].major;
 
 
+        })
+
+        var temp = 'https://college-app-io.herokuapp.com/getQuestions';
+        this.httpClient.get(temp,{headers: {'collegeName': this.message}}).subscribe(data => {
+              console.log(data);
+              this.questions = data;
         })
 
     })
@@ -107,30 +122,39 @@ export class CollegeSpecificPageComponent implements OnInit {
   }
 
   submitAppl(){
-    console.log(this.q1);
-    console.log(this.q2);
-    console.log(this.q3);
+    console.log("clicked submit");
+    console.log(this.answers);
+
     console.log(this.major);
     console.log(this.uid);
+    console.log(this.message);
+
+    var myString = "";
+    for(let questionsIter of this.answers){
+      if(questionsIter.trim().length > 0){
+        myString += questionsIter.trim();
+        myString += "||";
+      }
+    }
+    myString = myString.substring(0, myString.length - 2); 
+    console.log(myString);
 
     this.httpClient.post('https://college-app-io.herokuapp.com/postResponse', {
       collegeName: this.message,
       studentid: this.uid,
-      q1: this.q1,
-      q2: this.q2,
-      q3: this.q3,
-      appliedStatus: 1,
+      questions: myString,
+      appliedStatus: 0,
       major: this.major
     })
-    .subscribe(
+      .subscribe(
         res => {
           console.log(res);
         },
         err => {
           console.log("Error occured");
         }
-      )
-      ;
+      );
+      this.modalReferenceMainModal.close();
 
       var temp = 'http://college-app-io.herokuapp.com/sendEmail/'+this.email+'/'+this.message;
       this.httpClient.get(temp).subscribe(data => {
@@ -138,26 +162,31 @@ export class CollegeSpecificPageComponent implements OnInit {
       })
 
       this.isDisabled = true;
-      this.modalReference.close();
+      // this.modalReference.close();
       this.modalReferenceMainModal.close();
   }
 
   saveAppl(){
-    console.log(this.q1);
-    console.log(this.q2);
-    console.log(this.q3);
     console.log(this.major);
     console.log(this.uid);
+    console.log(this.message);
+
+    var myString = "";
+    for(let questionsIter of this.answers){
+      if(questionsIter.trim().length > 0){
+        myString += questionsIter.trim();
+        myString += "||";
+      }
+    }
+    myString = myString.substring(0, myString.length - 2); 
+    console.log(myString);
 
     this.httpClient.post('https://college-app-io.herokuapp.com/postResponse', {
       collegeName: this.message,
       studentid: this.uid,
-      q1: this.q1,
-      q2: this.q2,
-      q3: this.q3,
+      questions: myString,
       appliedStatus: 0,
       major: this.major
-
     })
       .subscribe(
         res => {
@@ -171,35 +200,39 @@ export class CollegeSpecificPageComponent implements OnInit {
   }
 
   confirm(contenttemp){
-      console.log(!this.q1 || 0 === this.q1.length);
-      if(!this.major || 0 === this.major.length){
-        console.log("no q1");
-        this.toggleq0 = false;
-      }else{
-        this.toggleq0 = true;
-      }
-      if(!this.q1 || 0 === this.q1.length){
-        console.log("no q1");
-        this.toggleq1 = false;
-      }else{
-        this.toggleq1 = true;
-      }
-      if(!this.q2 || 0 === this.q2.length){
-        console.log("no q1");
-        this.toggleq2 = false;
-      }else{
-        this.toggleq2 = true;
-      }
-      if(!this.q3 || 0 === this.q3.length){
-        console.log("no q1");
-        this.toggleq3 = false;
-      }else{
-        this.toggleq3 = true;
-      }
-      if(this.q1 && this.q2 && this.q3 && this.major){
-        console.log("cofirm");
-        this.modalReference = this.modalService.open(contenttemp, { centered: true });
-      }
+
+    console.log("clicked submit");
+    console.log(this.answers);
+
+      // console.log(!this.q1 || 0 === this.q1.length);
+      // if(!this.major || 0 === this.major.length){
+      //   console.log("no q1");
+      //   this.toggleq0 = false;
+      // }else{
+      //   this.toggleq0 = true;
+      // }
+      // if(!this.q1 || 0 === this.q1.length){
+      //   console.log("no q1");
+      //   this.toggleq1 = false;
+      // }else{
+      //   this.toggleq1 = true;
+      // }
+      // if(!this.q2 || 0 === this.q2.length){
+      //   console.log("no q1");
+      //   this.toggleq2 = false;
+      // }else{
+      //   this.toggleq2 = true;
+      // }
+      // if(!this.q3 || 0 === this.q3.length){
+      //   console.log("no q1");
+      //   this.toggleq3 = false;
+      // }else{
+      //   this.toggleq3 = true;
+      // }
+      // if(this.q1 && this.q2 && this.q3 && this.major){
+      //   console.log("cofirm");
+      //   this.modalReference = this.modalService.open(contenttemp, { centered: true });
+      // }
   }
 
   cancel(){
