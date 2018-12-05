8839d0cc6c1caa0bdc3e5eafa328983cb36af7f2
Harsha Lingareddy <hlingare@purdue.edu>
2018-12-04 22:33:04 -0500

    recomendaiton list

--- Frontend/collegeAppFrontEnd/src/app/home-page/home-page.component.ts
+++ Frontend/collegeAppFrontEnd/src/app/home-page/home-page.component.ts
@@ -21,7 +21,7 @@ export class HomePageComponent implements OnInit {
   uid: string;
   watchlist: any;
   addedToWatchList: boolean = true;
-
+  recommendedList: any;
 
   //collegeList: JSON;
   colleges: any;
@@ -73,6 +73,12 @@ export class HomePageComponent implements OnInit {
                 this.watchlist = data;
                 console.log(data);
           })
+          var url1 = 'http://college-app-io.herokuapp.com/getrecommendedColleges'
+          this.httpClient.get(url1,{headers: {'studentid':user.uid}}).subscribe(data => {
+            // this.userData = data[0] as JSON;
+            this.recommendedList = data;
+            console.log(data);
+      })
 
           var temp = 'https://college-app-io.herokuapp.com/getColleges';
             this.httpClient.get(temp,{headers: {'studentid':user.uid}}).subscribe(data => {
