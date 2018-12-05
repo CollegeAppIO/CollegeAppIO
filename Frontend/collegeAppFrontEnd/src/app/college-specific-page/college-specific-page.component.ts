b81dde4c38cc05291343e157fc6162dca23703d6
GokulsreeY <y.gokulsree@gmail.com>
2018-12-05 01:11:21 -0500

    added dv to college specific

--- Frontend/collegeAppFrontEnd/src/app/college-specific-page/college-specific-page.component.ts
+++ Frontend/collegeAppFrontEnd/src/app/college-specific-page/college-specific-page.component.ts
@@ -5,7 +5,7 @@ import { filter } from 'rxjs/operators';
 import { DataService } from "../data.service";
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../auth.service';
-
+import { Chart }  from 'chart.js';
 @Component({
   selector: 'app-college-specific-page',
   templateUrl: './college-specific-page.component.html',
@@ -35,6 +35,13 @@ export class CollegeSpecificPageComponent implements OnInit {
   email:string;
   modalReference: any;
   modalReferenceMainModal: any;
+  chart: Chart = [];
+  chart1: Chart = [];
+  chart2: Chart = [];
+  chart3: Chart = [];
+  chart4: Chart = [];
+  chart5: Chart = [];
+
   constructor(private httpClient: HttpClient, private route: ActivatedRoute, private data: DataService, private modalService: NgbModal,public authServ: AuthService) { }
 
   ngOnInit() {
@@ -108,6 +115,212 @@ export class CollegeSpecificPageComponent implements OnInit {
     //       console.log(this.userData);
     // })
 
+    var cat1: any;
+    var cat2: any;
+    var act: any;
+    var aps: any;
+    var temp1 = 'http://college-app-io.herokuapp.com/getStatsEachStudent';
+    this.httpClient.get(temp1,{headers: {'collegeName': this.message}}).subscribe(data1 => {
+      console.log(data1);
+           var actData = data1[0];
+           var satData = data1[1]
+           var numAPData = data1[2]
+           var gpaData = data1[3]
+           var raceData = data1[4]
+           var majorData = data1[5];
+           var sexData = data1[6];
+           var decisionData = data1[7];
+
+          cat1 = gpaData.map(gpaData => gpaData.gpa);
+          cat2 = satData.map(satData => satData.sat);
+          console.log(cat1);
+          console.log(cat2);
+          console.log(decisionData);
+          let coords = cat1.map( (v,i) => ({ x: v, y: cat2[i] }) )
+          var pointBackgroundColors = [];
+          this.chart = new Chart('canvas',{
+              type:'scatter',
+              data: {
+              datasets: [{
+                label: 'GPA vs SAT',
+                data: coords,
+                pointBackgroundColor: pointBackgroundColors
+              }
+
+              ]
+            },
+              options: {
+                scales: {
+                  xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                  }]
+                }
+              }
+          })
+
+
+          act = actData.map(actData => actData.act);
+          let coords1 = cat1.map( (v,i) => ({ x: v, y: act[i] }) )
+          console.log(coords1);
+          this.chart1 = new Chart('canvas1',{
+              type:'scatter',
+              data: {
+              datasets: [{
+                label: 'GPA vs ACT',
+                data: coords1,
+                pointBackgroundColor: pointBackgroundColors
+              }
+
+              ]
+            },
+              options: {
+                scales: {
+                  xAxes: [{
+                    type: 'linear',
+                    position: 'bottom',
+
+                  }]
+                },
+                showLine:'true'
+              }
+          })
+
+          aps = numAPData.map(numAPData => numAPData.num_ap);
+          let coords2 = cat1.map( (v,i) => ({ x: v, y: numAPData[i].num_ap }) )
+          console.log(coords2);
+          this.chart2 = new Chart('canvas2',{
+              type:'scatter',
+              data: {
+              datasets: [{
+                label: 'GPA vs Number of APs',
+                data: coords2,
+                pointBackgroundColor: pointBackgroundColors
+              }
+
+              ]
+            },
+              options: {
+                scales: {
+                  xAxes: [{
+                    type: 'linear',
+                    position: 'bottom',
+
+                  }]
+                },
+                showLine:'true'
+              }
+          })
+
+          var majorCounts = [];
+          var majorLabels = [];
+          var list = [];
+          var url1 = 'http://college-app-io.herokuapp.com/getCollegeCountMajor';
+          this.httpClient.get(url1,{headers: {'collegename': this.message}}).subscribe(data1 => {
+                console.log(data1);
+
+                for(var i = 0; i < Object.keys(data1).length; i++){
+                  majorCounts.push(data1[i].Count);
+                  majorLabels.push(data1[i].Major);
+                }
+                console.log(majorCounts);
+                console.log(majorLabels);
+                this.chart3 = new Chart('canvas3',{
+                  type: 'doughnut',
+                  data: {
+                      datasets: [{
+                          data: majorCounts,
+                          backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"]
+                      }],
+                      labels: majorLabels
+                  },
+                  options: {
+                      responsive: true,
+                      title:{
+                          display: true,
+                          text: "Major"
+                      }
+                  }
+              });
+
+          })
+          var raceCounts = [];
+          var raceLabels = [];
+          var url2 = 'http://college-app-io.herokuapp.com/getCollegeCountRace';
+          this.httpClient.get(url2,{headers: {'collegename': this.message}}).subscribe(data2 => {
+                console.log(data2);
+                for(var i = 0; i < Object.keys(data2).length; i++){
+                  raceCounts.push(data2[i].Count);
+                  raceLabels.push(data2[i].Race);
+                }
+                console.log(raceCounts);
+                console.log(raceLabels);
+                this.chart4 = new Chart('canvas4',{
+                  type: 'doughnut',
+                  data: {
+                      datasets: [{
+                          data: raceCounts,
+                          backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"]
+                      }],
+                      labels: raceLabels
+                  },
+                  options: {
+                      responsive: true,
+                      title:{
+                          display: true,
+                          text: "Major"
+                      }
+                  }
+              });
+
+          })
+          var sexCounts = [];
+          var sexLabels = [];
+          var url3 = 'http://college-app-io.herokuapp.com/getCollegeCountSex';
+          this.httpClient.get(url3,{headers: {'collegename': this.message}}).subscribe(data2 => {
+                console.log(data2);
+                for(var i = 0; i < Object.keys(data2).length; i++){
+                  sexCounts.push(data2[i].Count);
+                  sexLabels.push(data2[i].Sex);
+                }
+                console.log(sexCounts);
+                console.log(sexLabels);
+                this.chart5 = new Chart('canvas5',{
+                  type: 'doughnut',
+                  data: {
+                      datasets: [{
+                          data: sexCounts,
+                          backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"]
+                      }],
+                      labels: sexLabels
+                  },
+                  options: {
+                      responsive: true,
+                      title:{
+                          display: true,
+                          text: "Sex"
+                      }
+                  }
+              });
+
+          })
+          for (var i = 0; i < decisionData.length; i++) {
+            console.log(decisionData[i]);
+            if (decisionData[i].decision < 1) {
+              pointBackgroundColors.push("#00ff00");
+
+            } else {
+              console.log("else");
+              pointBackgroundColors.push("#CC0000");
+            }
+          }
+          this.chart.update();
+          this.chart1.update();
+          this.chart2.update();
+
+          console.log('what up');
+    })
+
 
   }
 
@@ -118,7 +331,7 @@ export class CollegeSpecificPageComponent implements OnInit {
 
           console.log(data);
     })
-    console.log(this.collegeName);
+    //console.log(this.collegeName);
   }
 
   open(content) {
